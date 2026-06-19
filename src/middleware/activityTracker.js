7dbadbb31865919540a2422a342a1e549bd9a416
Danosky8082 <dcmitch2000ng@gmail.com>
2026-06-19 17:19:18 +0100

    Minimal activityTracker – no res.send

--- src/middleware/activityTracker.js
+++ src/middleware/activityTracker.js
@@ -1,7 +1,5 @@
-// src/middleware/activityTracker.js
 module.exports = function activityTracker() {
   return function(req, res, next) {
-    // Only log – never send a response
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}`);
     next();
   };
